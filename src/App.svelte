b0b95f75113fe41898277df7537224a8991467d4
ericemdev-old <maina11682@student.mmarau.ac.ke>
2025-06-05 23:23:48 +0300

    line graph implementation

---
+++ src/App.svelte
@@ -0,0 +1,5 @@
+<script>
+    import Line from './components/line.svelte';
+</script>
+
+<Line />
---
+++ src/components/line.svelte
@@ -0,0 +1,40 @@
+<script>
+    import { chart } from 'svelte-apexcharts';
+
+
+    let options = {
+        chart: {
+            type: 'bar',
+            height: 'auto'
+        },
+        series: [{
+            name: 'Sales',
+            data: [30, 40, 35, 50, 49, 60, 70, 91, 125]
+        }],
+        xaxis: {
+            categories: [1991, 1992, 1993, 1994, 1995, 1996, 1997, 1998, 1999]
+        },
+        plotOptions: {
+            bar: {
+                borderRadius: 4,
+                columnWidth: '70%'
+            }
+        }
+    };
+</script>
+
+<div class="chart-container">
+    <div use:chart={options} />
+</div>
+
+<style>
+    .chart-container {
+        width: 100%;
+        max-width: 800px;
+        margin: 0 auto;
+    }
+
+    .chart-container > div {
+        min-height: 300px;
+    }
+</style>
